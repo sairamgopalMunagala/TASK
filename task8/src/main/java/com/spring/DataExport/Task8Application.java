e36f6781227189bf1dd3eb24982f26a68815741f
sairamgopalMunagala <munagalasairamgopal@gmail.com>
2024-06-10 10:20:43 +0530

    Add files via upload

---
+++ task8/src/main/java/com/spring/DataExport/Task8Application.java
@@ -0,0 +1,14 @@
+package com.spring.DataExport;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task8Application.class, args);
+	}
+
+}
